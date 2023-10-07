62e7ce320ae8515aede2ce279f69e0cf95192ec9
[zayn ali] <f200308@cfd.nu.edu.pk>
2023-10-07 19:59:07 +0500

    changes

---
+++ lab7/Calculator.java
@@ -0,0 +1,25 @@
+package lab7;
+
+public class Calculator {
+
+	public Calculator() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	int addfunction(int a,int b)
+	{
+		return a+b;	
+	}
+	
+	
+	int asubfunction(int a,int b)
+	{
+		return a-b;	
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
